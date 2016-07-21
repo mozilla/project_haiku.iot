04a3aae812ffd417f1e794388e1bf497d0d931b5
Manel Rhaiem <mrhaiem@mrhaiem-10117.local>
2016-07-21 22:21:40 +0100

    body json works

--- status-server/src/public/js/client.js
+++ status-server/src/public/js/client.js
@@ -12,19 +12,36 @@ function pollStatus() {
       catch (e) {}
     }
   }
+
    // make a request to /status.json every 10s
  window.fetch('http://localhost:3000/status.json').then(function(response) {
-  console.log(response.statusText);
+  //console.log(response.statusText);
+  var value;
+  document.getElementById("serverStatus").innerHTML = response.statusText;
+
+  response.json().then(function(json) {
+    console.log(json.value);
+     value= json.status.value;
+   });
+  updateStatus(response.statusText,value);
 });
-  // call updateStatus in the callback
-  var intervalID = setInterval(function() { pollStatus(); }, 2000);
+//  var intervalID = setInterval(function() { pollStatus(); }, 2000);
 }
 
-function updateStatus() {
+function updateStatus(currentresponse,value) {
   // store updated value, did the value change?
+  window.fetch('http://localhost:3000/status.json').then(function(response) {
+   if (response.statusText == currentresponse){
+     var led = document.getElementById("led0");
+
+     console.log(led);
+     console.log(value);
+     //renderStatus(led,value);
+   }
+ });
   // call render if value or last-modified changed
 }
 function renderStatus(idx, value) {
-  // could mock some LED sequences here?
+  // could mock some LED sequ  ences here?
   // and/or just print the status value and last-modified date for now
 }
