c421d195404a537af04a4b0aea4fda7647226ca7
Manel Rhaiem <mrhaiem@mrhaiem-10117.local>
2016-07-21 09:17:22 +0100

    pollStatus test

--- status-server/src/public/js/client.js
+++ status-server/src/public/js/client.js
@@ -1,7 +1,25 @@
 function pollStatus() {
-  // make a request to /status.json every 10s
+  if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    request = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { // IE
+    try {
+      request = new ActiveXObject('Msxml2.XMLHTTP');
+    }
+    catch (e) {
+      try {
+        request = new ActiveXObject('Microsoft.XMLHTTP');
+      }
+      catch (e) {}
+    }
+  }
+   // make a request to /status.json every 10s
+ window.fetch('http://localhost:3000/status.json').then(function(response) {
+  console.log(response.statusText);
+});
   // call updateStatus in the callback
+  var intervalID = setInterval(function() { pollStatus(); }, 2000);
 }
+
 function updateStatus() {
   // store updated value, did the value change?
   // call render if value or last-modified changed
