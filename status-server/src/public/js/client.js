752d1010da525b2ba71ad4eb7d13f43e40f1f9ec
mermi <manel.rhaiem92@gmail.com>
2016-07-28 11:45:30 +0100

    add the post request to change status and blink the Led

--- status-server/src/public/js/client.js
+++ status-server/src/public/js/client.js
@@ -42,15 +42,78 @@ function init() {
     }
   }
   // populate the LEDs with some initial colors
-  var colors = ['#ffff00', '#ffff00', '#00ffff', '#00ff07', '#772cb6'];
+  var colors = ['#ff00ff', '#ffff00', '#00ffff', '#00ff07', '#772cb6'];
   colors.forEach((color, idx) => {
     var node = document.getElementById("led" + idx);
     var ctx = node.getContext("2d");
     ctx.fillStyle = color;
     ctx.fillRect(0,0,300,300);
+    ctx.font = "10px Arial";
+    ctx.strokeText(idx ,node.width/2,node.height/2);
+
+
   });
 }
+function animate(ct){
+  var fetchResponse = window.fetch('http://localhost:3000/status'+ct+'.json'+Date.now()).then(
+      function onSuccess(response){
+        return response.json();
+      },
+      function onError(err){
+        console.console.warn("Error fetching"+ url, err);
+      }
+    );
 
+
+   updateValueStatus(ct);
+   //blinkStatus(ct);
+
+}
+
+function updateValueStatus(ct){
+  // var ch = '/status'+ct+'.json';
+  // console.log(ch);
+  // var statusUpdate = {ch: {} };
+  // console.log(statusUpdate);
+  // var urlKey = Object.keys(statusUpdate);
+  // console.log(urlKey);
+  // console.log(statusItems);
+  var statusData = statusItems['/status'+ct+'.json'];
+  console.log(statusData);
+  var value = statusData.value;
+  var didChange;
+  var lastModified = statusData['last-modified'];
+  if(value == 1){
+    value = 0;
+    didChange=true;
+  } else if (value === 0){
+    value =1;
+    didChange=true;
+  }else{
+    didChange = false;
+  }
+  statusData.value = value;
+  statusData.lastModified = Date.now();
+  statusData.didChange = didChange;
+}
+function blinkStatus(ct){
+    var statusData = statusItems['/status'+ct+'.json'];
+    if (statusData.didChange) {
+      console.log('render blink status:', '/status'+ct+'.json', statusData);
+      var nodeSelected = document.getElementsByName("iframeS1").contentWindow.getElementById("led"+ct).getContext("2d");
+      var color;
+      switch (statusData.value) {
+        case '0':
+          color = 'red';
+          break;
+        case '1':
+          color = 'green';
+          break;
+      }
+      nodeSelected.fillStyle = color;
+      nodeSelected.fillRect(0,0,40,40);
+    }
+}
 function requestStatus() {
   var fetchResponses = Object.keys(statusItems).map(url => {
     // make a request to /status.json every 10s
