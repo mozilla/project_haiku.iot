a84885e432a336342d83d01b2f200ee3b20387b3
Kian Solomon <kian.solomon@Yahoo.com>
2016-08-01 16:58:42 -0700

    changed routes & reformatted node

--- status-server/src/index.js
+++ status-server/src/index.js
@@ -10,7 +10,6 @@ var dataDir = path.join(__dirname, '../data');
 var publicDir = path.join(__dirname, 'public');
 var config = {
   port: 3000,
-  prefix: 'status'
 };
 // mixin command-line args into our default config
 Object.keys(config).forEach(function(name) {
@@ -25,11 +24,10 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(express.static(publicDir));
-
-app.get('/'+config.prefix+':num\.:ext?', function (req, res) {
-  var num = req.params.num || 0;
-  var filename = path.join(dataDir, config.prefix + num);
-  var ext = req.params.ext || '';
+//handles JSON requests based on the :id status number. reads file of status number provided and returns the data in json format
+app.get('/api/user/:id/status', function(req, res){
+  var num = req.params.id || 0;
+  var filename = path.join(dataDir, 'status' + num);
   fs.readFile(filename, function(err, buf) {
     if (err) {
       console.error(err.stack);
@@ -42,26 +40,35 @@ app.get('/'+config.prefix+':num\.:ext?', function (req, res) {
         return res.status(500).send('Error reading status last-modified!');
       }
       res.setHeader('Last-Modified', stats.mtime);
-      switch (ext.toLowerCase()) {
-        case 'json':
-          res.json({ 'last-modified': (new Date(stats.mtime)).getTime(), value: status });
-          break;
-        case 'html':
-          res.set('Content-Type', 'text/html');
-          res.send( htmlResponse({ url: req.path, name: 'status', value: status }));
-          break;
-        default:
-          res.send(status);
-      }
+      res.json({ 'last-modified': (new Date(stats.mtime)).getTime(), value: status });
     });
-
   });
 });
-
-app.post('/'+config.prefix+':num\.:ext?', function (req, res) {
-  var num = req.params.num || 0;
-  var filename = path.join(dataDir, config.prefix + num);
-  var ext = req.params.ext || '';
+//handles html requests based on :id, the status number. reads file of :id provided and renders html page with an update bar to update status
+app.get('/user/:id/status', function (req, res) {
+  var num = req.params.id || 0;
+  var filename = path.join(dataDir, 'status' + num);
+  fs.readFile(filename, function(err, buf) {
+    if (err) {
+      console.error(err.stack);
+      return res.status(500).send('Error reading status!');
+    }
+    var status = buf.toString()
+    fs.stat(filename, function(err, stats) {
+      if (err) {
+        console.error(err.stack);
+        return res.status(500).send('Error reading status last-modified!');
+      }
+      res.setHeader('Last-Modified', stats.mtime);
+      res.set('Content-Type', 'text/html');
+      res.send( htmlResponse({ url: req.path, name: 'status', value: status }));
+    });
+  });
+});
+//handles the post request for status. writes data to prefix{id} file and updates html page with submitted data
+app.post('/api/user/:id/status', function (req, res) {
+  var num = req.params.id || 0;
+  var filename = path.join(dataDir, 'status' + num);
   var status = req.body.value;
   fs.writeFile(filename, status, function(err, buf) {
     if (err) {
@@ -74,19 +81,8 @@ app.post('/'+config.prefix+':num\.:ext?', function (req, res) {
         return res.status(500).send('Error reading status last-modified!');
       }
       res.setHeader('Last-Modified', stats.mtime);
-      switch (ext.toLowerCase()) {
-        case 'json':
-          res.json({ 'last-modified': stats.mtime, ok: true });
-          break;
-        case 'html':
-          res.set('Content-Type', 'text/html');
-          res.send( htmlResponse({ url: req.path, name: 'status', value: status }));
-          break;
-        default:
-          res.send('OK');
-      }
+      res.json({ 'last-modified': stats.mtime, ok: true });
     });
-
   });
 });
 
@@ -101,7 +97,7 @@ function htmlResponse(ctxData) {
   '<html><head><title>%{name}</title></head>\n' +
   '<body>' +
   '<h1>%{name}: %{value}</h1>' +
-  '<form action="%{url}" method="POST">' +
+  '<form action="/api%{url}" method="POST">' +
   '<input name="value" value="%{value}">' +
   '<input type="submit" value="Update">' +
   '</form></body></html>';
