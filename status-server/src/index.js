817e0e7b3be9e5eed5ab89ae9da0c62106cfc7a6
Kian Solomon <kian.solomon@Yahoo.com>
2016-08-03 13:51:38 -0700

    updated(2) routing issue #64

--- status-server/src/index.js
+++ status-server/src/index.js
@@ -10,6 +10,7 @@ var dataDir = path.join(__dirname, '../data');
 var publicDir = path.join(__dirname, 'public');
 var config = {
   port: 3000,
+  prefix: 'api'
 };
 // mixin command-line args into our default config
 Object.keys(config).forEach(function(name) {
@@ -24,8 +25,9 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(express.static(publicDir));
-//handles JSON requests based on the :id status number. reads file of status number provided and returns the data in json format
-app.get('/api/user/:id/status', function(req, res){
+
+//application/json: handles JSON requests based on the :id status number. reads file of status number provided and returns the data in json format
+app.get('/api/user/:id/status.json', function(req, res){
   var num = req.params.id || 0;
   var filename = path.join(dataDir, 'status' + num);
   fs.readFile(filename, function(err, buf) {
@@ -44,7 +46,7 @@ app.get('/api/user/:id/status', function(req, res){
     });
   });
 });
-//handles html requests based on :id, the status number. reads file of :id provided and renders html page with an update bar to update status
+//text/plain: handles html requests based on :id, the status number. reads file of :id provided and renders html page with an update bar to update status
 app.get('/user/:id/status', function (req, res) {
   var num = req.params.id || 0;
   var filename = path.join(dataDir, 'status' + num);
@@ -61,7 +63,28 @@ app.get('/user/:id/status', function (req, res) {
       }
       res.setHeader('Last-Modified', stats.mtime);
       res.set('Content-Type', 'text/html');
-      res.send( htmlResponse({ url: req.path, name: 'status', value: status }));
+      res.send( htmlResponse({ prefix: '/' + config.prefix, path: req.path, name: 'status', value: status }));
+    });
+  });
+});
+//text/html
+app.get('/user/:id/status.html', function (req, res) {
+  var num = req.params.id || 0;
+  var filename = path.join(dataDir, 'status' + num);
+  fs.readFile(filename, function(err, buf) {
+    if (err) {
+      console.error(err.stack);
+      return res.status(500).send('Error reading status!');
+    }
+    var status = buf.toString()
+    fs.stat(filename, function(err, stats) {
+      if (err) {
+        console.error(err.stack);
+        return res.status(500).send('Error reading status last-modified!');
+      }
+      res.setHeader('Last-Modified', stats.mtime);
+      res.set('Content-Type', 'text/html');
+      res.send( htmlResponse({ prefix: '/' + config.prefix, path: req.path, name: 'status', value: status }));
     });
   });
 });
@@ -86,6 +109,45 @@ app.post('/api/user/:id/status', function (req, res) {
   });
 });
 
+app.post('/api/user/:id/status.html', function (req, res) {
+  var num = req.params.id || 0;
+  var filename = path.join(dataDir, 'status' + num);
+  var status = req.body.value;
+  fs.writeFile(filename, status, function(err, buf) {
+    if (err) {
+      console.error(err.stack);
+      return res.status(500).send('Error updating status!');
+    }
+    fs.stat(filename, function(err, stats) {
+      if (err) {
+        console.error(err.stack);
+        return res.status(500).send('Error reading status last-modified!');
+      }
+      res.setHeader('Last-Modified', stats.mtime);
+      res.json({ 'last-modified': stats.mtime, ok: true });
+    });
+  });
+});
+//json extension route
+app.post('/api/user/:id/status.json', function (req, res) {
+  var num = req.params.id || 0;
+  var filename = path.join(dataDir, 'status' + num);
+  var status = req.body.value;
+  fs.writeFile(filename, status, function(err, buf) {
+    if (err) {
+      console.error(err.stack);
+      return res.status(500).send('Error updating status!');
+    }
+    fs.stat(filename, function(err, stats) {
+      if (err) {
+        console.error(err.stack);
+        return res.status(500).send('Error reading status last-modified!');
+      }
+      res.setHeader('Last-Modified', stats.mtime);
+      res.json({ 'last-modified': stats.mtime, ok: true });
+    });
+  });
+});
 app.use(serveIndex(publicDir, {'icons': true}));
 
 app.listen(config.port, function () {
@@ -97,7 +159,7 @@ function htmlResponse(ctxData) {
   '<html><head><title>%{name}</title></head>\n' +
   '<body>' +
   '<h1>%{name}: %{value}</h1>' +
-  '<form action="/api%{url}" method="POST">' +
+  '<form action="%{prefix}%{path}" method="POST">' +
   '<input name="value" value="%{value}">' +
   '<input type="submit" value="Update">' +
   '</form></body></html>';
