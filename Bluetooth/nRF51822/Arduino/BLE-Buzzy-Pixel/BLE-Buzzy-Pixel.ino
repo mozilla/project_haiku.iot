39a19ba31142406b0173c56ba69d8db01ac8df0c
Jim Porter <jporter@mozilla.com>
2016-08-26 17:31:28 -0500

    Clean up the Buzzy Pixel code

--- Bluetooth/nRF51822/Arduino/BLE-Buzzy-Pixel/BLE-Buzzy-Pixel.ino
+++ Bluetooth/nRF51822/Arduino/BLE-Buzzy-Pixel/BLE-Buzzy-Pixel.ino
@@ -1,112 +1,113 @@
-// Import libraries (BLEPeripheral depends on SPI)
+// Import libraries (BLEPeripheral depends on SPI).
 #include <SPI.h>
 #include <BLEPeripheral.h>
 #include <Adafruit_NeoPixel.h>
 
+#include <cstring>
+
 #include "config.h"
 
 // Create NeoPixel strip from parameters in config.h.
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+Adafruit_NeoPixel strip(SLOT_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
-// Create peripheral instance, see pinouts in config.h
-BLEPeripheral            blePeripheral        = BLEPeripheral(BLE_REQ, BLE_RDY, BLE_RST);
+// Create peripheral instance, see pinouts in config.h.
+BLEPeripheral blePeripheral(BLE_REQ, BLE_RDY, BLE_RST);
 
-// uuid's can be:
-//   16-bit: "ffff"
-//  128-bit: "19b10010e8f2537e4f6cd104768a1214" (dashed format also supported)
+// Create service.
+BLEService bleBuzzService(BLE_UUID);
 
-// create service
-BLEService               bleBuzzService           = BLEService("0318e986-54b5-11e6-beb8-9e71128cae77");
+// Create switch and button characteristic and one or more descriptors.
+BLECharacteristic pushStatusCharacteristic(
+  BLE_UUID, BLERead | BLEWrite, SLOT_COUNT
+);
+BLEDescriptor pushStatusDescriptor("2901", "Push LED Status");
 
-// create switch and button characteristic and one or more descriptors (optional)
-BLECharacteristic    pushStatusCharacteristic = BLECharacteristic("0318ef80-54b5-11e6-beb8-9e71128cae77", BLERead | BLEWrite, BUF_LEN);
-BLEDescriptor pushStatusDescriptor = BLEDescriptor("2901", "Push LED Status");
+BLECharCharacteristic readStatusCharacteristic(BLE_UUID, BLERead | BLENotify);
+BLEDescriptor readStatusDescriptor("2901", "Read-BLE-Device-Status");
 
-BLECharCharacteristic    readStatusCharacteristic = BLECharCharacteristic("0318f084-54b5-11e6-beb8-9e71128cae77", BLERead | BLENotify);
-BLEDescriptor readStatusDescriptor = BLEDescriptor("2901", "Read-BLE-Device-Status");
-
-const unsigned char initialStatus[BUF_LEN] = {0x01,0x01,0x01,0x01,0x01,0x01,0x01};
-int currentState;
-int debounceState;
 int buttonState = 0;
-int selfLED = 4; //TODO: Move to config.h
 
 void setup() {
   Serial.begin(9600);
 
-  // set  button pin to input mode
+  // Set button pin to input mode.
   pinMode(BUTTON_PIN, INPUT);
 
-   // Initialize NeoPixels.
+  // Initialize NeoPixels.
   strip.begin();
   strip.show();
 
-  // set advertised name and service
+  // Set advertised name and service.
   blePeripheral.setDeviceName("BLE-Buzzy-Pixel " DEVICE_ID);
   blePeripheral.setLocalName("BLE-Buzzy-Pixel " DEVICE_ID);
   blePeripheral.setAdvertisedServiceUuid(bleBuzzService.uuid());
 
-  // add service and characteristics
+  // Add service and characteristics.
   blePeripheral.addAttribute(bleBuzzService);
   blePeripheral.addAttribute(pushStatusCharacteristic);
   blePeripheral.addAttribute(pushStatusDescriptor);
   blePeripheral.addAttribute(readStatusCharacteristic);
   blePeripheral.addAttribute(readStatusDescriptor);
 
-
+  unsigned char initialStatus[SLOT_COUNT];
+  std::memset(initialStatus, 0x01, sizeof(initialStatus));
   pushStatusCharacteristic.setValue(initialStatus, sizeof(initialStatus));
+
   readStatusCharacteristic.setValue(0);
 
-  pushStatusCharacteristic.setEventHandler(BLEWritten, pushStatusCharacteristicWritten);
+  pushStatusCharacteristic.setEventHandler(
+    BLEWritten, pushStatusCharacteristicWritten
+  );
 
-  // begin initialization
+  // Begin initialization.
   blePeripheral.begin();
 
   Serial.println(F("BLE Buzz Peripheral"));
 }
 
 void loop() {
-  // poll peripheral
   blePeripheral.poll();
   processButtonChange();
 }
 
 void processButtonChange() {
-  // read the current button pin state
-  currentState = digitalRead(BUTTON_PIN);
+  // Read the current button pin state.
+  int currentState = digitalRead(BUTTON_PIN);
   delay(10);
-  debounceState = digitalRead(BUTTON_PIN);
+  int debounceState = digitalRead(BUTTON_PIN);
 
-  if(currentState == debounceState) {
-    if(currentState != buttonState) {
-      if(currentState == LOW) {
-        // button released
-      } else {
-         // toggle the value since the last status
-        if(readStatusCharacteristic.value() == 0x00) {
-          readStatusCharacteristic.setValue(0x01);
-        } else {
-          readStatusCharacteristic.setValue(0x00);
-        }
-        Serial.println("button Status");
-        Serial.println(readStatusCharacteristic.value(),DEC);
-        renderStatus(readStatusCharacteristic.value(), selfLED);
-      }
-      buttonState = currentState;
-    }
+  if(currentState != debounceState) {
+    return;
   }
+
+ if(currentState != buttonState) {
+   if(currentState != LOW) {
+     // toggle the value since the last status
+     if(readStatusCharacteristic.value() == 0x00) {
+       readStatusCharacteristic.setValue(0x01);
+     } else {
+       readStatusCharacteristic.setValue(0x00);
+     }
+     Serial.println("button Status");
+     Serial.println(readStatusCharacteristic.value(), DEC);
+     renderStatus(readStatusCharacteristic.value(), SELF_LED);
+   }
+   buttonState = currentState;
+ }
 }
 
-void pushStatusCharacteristicWritten(BLECentral& central, BLECharacteristic& characteristic){
+void pushStatusCharacteristicWritten(
+  BLECentral& central, BLECharacteristic& characteristic
+) {
   updateStatus();
 }
 
-void updateStatus(){
-  //Get status array from the characteristic
+void updateStatus() {
+  // Get status array from the characteristic.
   const unsigned char* status = pushStatusCharacteristic.value();
-  for(int i=0;i<BUF_LEN; i++) {
+  for(int i = 0; i != SLOT_COUNT; i++) {
     Serial.println(status[i], DEC);
-    if (status[i] == 0x01) {
+    if(status[i] == 0x01) {
       Serial.println(F("LED on"));
       renderStatus(1, i);
     } else {
@@ -117,18 +118,18 @@ void updateStatus(){
 }
 
 void renderStatus(int color, int pixel){
-  if (color == 0) {
-     // Reset to red, off
-     animatePixels(strip, 255, 0, 0, pixel);
+  if(color == 0) {
+    // Reset to red, off.
+    animatePixels(strip, 255, 0, 0, pixel);
   } else {
-    // Turn neo pixel green and animate
-     animatePixels(strip, 0, 255, 0, pixel);
+    // Turn neo pixel green and animate.
+    animatePixels(strip, 0, 255, 0, pixel);
   }
-
 }
 
-void animatePixels(Adafruit_NeoPixel& strip, uint8_t r, uint8_t g, uint8_t b, int pixel) {
-  // Set NeoPixels Color
+void animatePixels(Adafruit_NeoPixel& strip, uint8_t r, uint8_t g, uint8_t b,
+                   int pixel) {
+  // Set NeoPixels Color.
   strip.setPixelColor(pixel, r, g, b);
   strip.show();
   delay(100);
