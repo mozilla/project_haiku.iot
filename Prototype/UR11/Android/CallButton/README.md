524f49866b1ec26f667ed983b342fa7d512528e2
Punam Dahiya <punamdahiya@yahoo.com>
2016-09-21 15:31:14 -0700

    Call Button android native app

---
+++ Prototype/UR11/Android/CallButton/README.md
@@ -0,0 +1,13 @@
+# Project Haiku - UR #11
+
+#### Android Call Button app
+
+A simple app that let user call a pre-configured contact on click of button. To simplify testing enter the phone number in the text box provided and click Call. 
+
+Call Button app supports Android 5.0 and above. Tested install with Android Studio 2.1.3
+
+Useful link
+* [Creating Android App](https://developer.android.com/training/basics/firstapp/creating-project.html)
+* [Requesting permission in Android 6.0 (API level 23 and above)](https://developer.android.com/training/permissions/requesting.html)
+
+
---
+++ Prototype/UR11/Android/CallButton/app/src/main/java/com/haiku/callbutton/MainActivity.java
@@ -0,0 +1,163 @@
+package com.haiku.callbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.os.Build;
+import android.Manifest;
+import android.support.v4.content.ContextCompat;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button callBtn;
+    private EditText number;
+    private static final int MY_PERMISSIONS_CALL_PHONE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number = (EditText) findViewById(R.id.phone);
+        callBtn = (Button) findViewById(R.id.call);
+
+        // add PhoneStateListener for monitoring
+        MyPhoneListener phoneListener = new MyPhoneListener(this);
+        TelephonyManager telephonyManager =
+                (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        // receive notifications of telephony state changes
+        telephonyManager.listen(phoneListener,PhoneStateListener.LISTEN_CALL_STATE);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            getPermissionToCallPhone();
+        }
+
+        callBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                try {
+                    String uri = "";
+                    // set the data
+                    if("".equals(number.getText().toString())) {
+                        uri = "tel:6507599066";
+                    } else {
+                        uri = "tel:" + number.getText().toString();
+                    }
+
+                    Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse(uri));
+                    startActivity(callIntent);
+                } catch(Exception e) {
+                    Toast.makeText(getApplicationContext(),"Your call has failed...",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private class MyPhoneListener extends PhoneStateListener {
+
+        private boolean onCall = false;
+        Context context;
+        AudioManager audioManager;
+        public MyPhoneListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            switch (state) {
+                case TelephonyManager.CALL_STATE_RINGING:
+                    // phone ringing...
+                    Toast.makeText(MainActivity.this, incomingNumber + " calls you",
+                            Toast.LENGTH_LONG).show();
+                    break;
+
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    // one call exists that is dialing, active, or on hold
+                    Toast.makeText(MainActivity.this, "on call...",
+                            Toast.LENGTH_LONG).show();
+                    //because user answers the incoming call
+                    onCall = true;
+                    try {
+                        Thread.sleep(500); // Delay 0,5 seconds to handle better turning on loudspeaker
+                    } catch (InterruptedException e) {
+                    }
+
+                    //Activate loudspeaker
+                    audioManager = (AudioManager)
+                            getSystemService(Context.AUDIO_SERVICE);
+                    audioManager.setMode(AudioManager.MODE_IN_CALL);
+                    audioManager.setSpeakerphoneOn(true);
+                    break;
+
+                case TelephonyManager.CALL_STATE_IDLE:
+                    // in initialization of the class and at the end of phone call
+
+                    // detect flag from CALL_STATE_OFFHOOK
+                    if (onCall == true) {
+                        Toast.makeText(MainActivity.this, "restart app after call",
+                                Toast.LENGTH_LONG).show();
+                        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                        audioManager.setMode(AudioManager.MODE_NORMAL); //Deactivate loudspeaker
+
+                        // restart our application
+                        Intent restart = getBaseContext().getPackageManager().
+                                getLaunchIntentForPackage(getBaseContext().getPackageName());
+                        restart.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(restart);
+
+                        onCall = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    public void getPermissionToCallPhone() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_CALL_PHONE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_CALL_PHONE: {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Call Phone Permission Refused",
+                            Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'switch' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+}
