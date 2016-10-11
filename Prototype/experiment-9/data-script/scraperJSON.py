d81ae79c91e12ed89183c42bcb74187f4ce555c3
Sam Foster <sfoster@mozilla.com>
2016-10-11 09:59:32 -0700

    Add lookup, to map numbers to device ids. Add datetime field. Send output to stdout

--- Prototype/experiment-9/data-script/scraperJSON.py
+++ Prototype/experiment-9/data-script/scraperJSON.py
@@ -1,81 +1,64 @@
-import PyPDF2 
+import PyPDF2
 import json
 import sys
+import re
+import time
+import dateutil.parser
 
 pdfFileObj = open(sys.argv[1], 'rb')
-textFile = open(sys.argv[1]+'.json', 'w')
+
+lookupFileObj = open('./lookup.json')
+lookupData = json.load(lookupFileObj)
+numbersToIds = {}
+for i in lookupData.keys():
+    numbersToIds[lookupData[i]] = i
+
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 num_pages = pdfReader.numPages
-currNum = sys.argv[1].split('-')[0:3]
-currNum[2]= currNum[2][0:4]
-currPhoneNum = ''
 
-for i in currNum:
-    currPhoneNum += i
+filename = sys.argv[1]
+phoneNumberPattern = re.compile('([\d\-]{10,})')
+currPhoneNum = 'unknown'
+numberMatch = phoneNumberPattern.search(filename)
+if (numberMatch):
+    currPhoneNum = numberMatch.group(1)
+
 incomingCall = []
 outgoingCall = []
 incomingSMS = []
 outgoingSMS = []
 data = {}
-myPhone = {}
 
 for x in range(0, num_pages):
     current_page = pdfReader.getPage(x).extractText()
     currentData = current_page.splitlines()
     for idx, i in enumerate(currentData):
+        # time.strptime(str,fmt='%a %b %d %H:%M:%S %Y')
         if 'Outgoing' in i or 'Incoming' in i:
+            dateValue = currentData[idx+2]
+            timeValue = currentData[idx+3]
+            dateTimeStr = dateutil.parser.parse(dateValue+' '+timeValue).isoformat()
+            entry = {
+                'time': timeValue,
+                'contact': currentData[idx+1],
+                'date': dateValue,
+                'duration': currentData[idx+4],
+                'datetime': dateTimeStr
+            }
             if 'Incoming Call' in i:
-                incomingCall.append({'time': currentData[idx+3], 'contact': currentData[idx+1],
-                'date': currentData[idx+2], 'duration': currentData[idx+4]})
+                incomingCall.append(entry)
             elif 'Outgoing Call' in i:
-                outgoingCall.append({'time': currentData[idx+3], 'contact': currentData[idx+1],
-                'date': currentData[idx+2], 'duration': currentData[idx+4]})
+                outgoingCall.append(entry)
             elif 'Incoming Text' in i:
-                incomingSMS.append({'time': currentData[idx+3], 'contact': currentData[idx+1], 
-                'date': currentData[idx+2], 'duration': currentData[idx+4]})
+                incomingSMS.append(entry)
             elif 'Outgoing Text' in i:
-                outgoingSMS.append({'time': currentData[idx+3], 'contact': currentData[idx+1],
-                'date': currentData[idx+2], 'duration': currentData[idx+4]})
+                outgoingSMS.append(entry)
+
 data['incomingCall'] = incomingCall
 data['outgoingCall'] = outgoingCall
 data['incomingSMS'] = incomingSMS
 data['outgoingSMS'] = outgoingSMS
-#print(json.dumps(data))
-myPhone[currPhoneNum] = data
-textFile.write(json.dumps(myPhone))
-textFile.close()
-#print(json.dumps(myPhone))
-#{
-#    'phoneNumber': {
-#
-#        'IncomingCall': [{
-#            'time': data,
-#            'date': data, 
-#            'contact': data,
-#            'duration': data
-#            },
-#           {
-#            'time': data,
-#            'date': data, 
-#            'contact': data,
-#            'duration': data
-#           }],
-#
-#        'OutgoingCall': [{
-#            'timestamp': data,
-#            'date': data, 
-#            'contact': data,
-#            'duration': data
-#        }],
-#        'IncomingSMS': [{
-#            'timestamp': data,
-#            'date': data, 
-#            'contact': data
-#        }],
-#        'OutgoingSMS': [{
-#            'timestamp': data,
-#            'date': data, 
-#            'contact': data
-#        }]
-#    }
-#}
+data['phoneNumber'] = currPhoneNum
+data['deviceId'] = numbersToIds[currPhoneNum] if numberMatch else 'unknown'
+
+print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
