d5ced6e1e3455dc719dd7b11dba2740d63f175bc
Sam Foster <sfoster@mozilla.com>
2016-10-13 15:36:36 -0700

    Updated data files, script now writes out the anonymised filename. Can now run the shell script from any directory

--- Prototype/experiment-9/data-script/convert-to-json.sh
+++ Prototype/experiment-9/data-script/convert-to-json.sh
@@ -3,9 +3,9 @@
 # Usage
 # find /path/to/PDF/directory -type f -name "*.pdf" -print0 | xargs -0 ./convert-to-json.sh
 #
-
+DIR=`dirname $0`
 for PDF in "$@"
 do
   outfile=`echo $PDF | sed 's/\(.*\.\)pdf/\1json/'`
-  python scraperJSON.py "$PDF"
+  python $DIR/scraperJSON.py "$PDF"
 done
